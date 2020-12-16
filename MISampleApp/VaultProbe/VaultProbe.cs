896339f3f078816244274f3971817823d70a22d9
amenarde <anmenard@microsoft.com>
2020-12-15 16:02:32 -0800

    adds demonstration of using ManagedIdentityCredential

--- MISampleApp/VaultProbe/VaultProbe.cs
+++ MISampleApp/VaultProbe/VaultProbe.cs
@@ -12,6 +12,9 @@ namespace Azure.ServiceFabric.ManagedIdentity.Samples
     using Newtonsoft.Json;
     using System.Security.Cryptography.X509Certificates;
     using System.Net.Security;
+    using Azure.Identity;
+    using Azure.Security.KeyVault.Secrets;
+    using Azure.Core;
 
 
     /// <summary>
@@ -99,19 +102,81 @@ namespace Azure.ServiceFabric.ManagedIdentity.Samples
         /// Probe a vault secret according to default config.
         /// </summary>
         /// <returns></returns>
-        public Task<string> ProbeSecretAsync()
+        public async Task<string> ProbeSecretAsync()
         {
-            return ProbeSecretAsync(config.VaultName, config.SecretName, String.Empty);
+            
+            // Demonstrates using Azure.Identity to abstract away tokens, and use the managed identity credential to speak to KeyVault
+            string azIdFetchedSecret = await ProbeSecretWithManagedIdentityCredentialAsync(config.VaultName, config.SecretName);
+
+            // Demonstrates using ManagedIdentityTokenService to fetch a token, cache, and present to KeyVault
+            string mitsTokenFetchedSecret = await ProbeSecretAsync(config.VaultName, config.SecretName, String.Empty);
+
+            return $"Token fetched secret:\n{mitsTokenFetchedSecret}\n\nManagedIdentityCredential fetched secret:\n{azIdFetchedSecret}";
         }
 
         /// <summary>
-        /// Probe the specified secret, displaying metadata on success.  
+        /// Probe the specified secret using ManagedIdentityCredential, displaying metadata on success.  
         /// </summary>
         /// <param name="vault">vault name</param>
         /// <param name="secret">secret name</param>
+        /// <returns></returns>
+        public async Task<string> ProbeSecretWithManagedIdentityCredentialAsync(string vaultUri, string secretName)
+        {
+
+            string response;
+
+            try
+            {
+                ManagedIdentityCredential creds;
+                KeyVaultSecret secret;
+
+                try
+                {
+                    // Get a credential representing the service's SF Application Identity
+                    creds = new ManagedIdentityCredential();
+
+                    // Throw away token to allow early failures
+                    creds.GetToken(new TokenRequestContext(new[] { "https://vault.azure.net" }));
+                }
+                catch (CredentialUnavailableException e)
+                {
+                    response = $"0x{e.HResult:X}: {e.Message} Encountered an exception accessing the service's managed identity. Was it deployed to Azure with an identity?";
+                    Log(LogLevel.Info, response);
+                    return response;
+                }
+
+                try
+                {
+                    SecretClient client = new SecretClient(new Uri(vaultUri), creds);
+                    secret = (await client.GetSecretAsync(secretName)).Value;
+                    response = PrintKeyVaultSecretMetadata(secret);
+                }
+                catch (RequestFailedException e)
+                {
+                    response = $"0x{e.HResult:X}: Status={e.Status} {e.Message} Encountered an exception fetching secret {secretName} from vault {vaultUri}";
+                    Log(LogLevel.Info, response);
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                // handle generic errors here
+                response = $"0x{e.HResult:X}: {e.Message} Encountered an exception fetching secret {secretName} from vault {vaultUri}";
+            }
+
+            Log(LogLevel.Info, response);
+            return response;
+        }
+
+        /// <summary>
+        /// Probe the specified secret using a token from ManagedIdentityTokenService, displaying metadata on success.  
+        /// </summary>
+        /// <param name="vaultUri">vault name</param>
+        /// <param name="Name">secret name</param>
         /// <param name="version">secret version id</param>
         /// <returns></returns>
-        public async Task<string> ProbeSecretAsync(string vault, string secret, string version)
+        public async Task<string> ProbeSecretAsync(string vaultUri, string Name, string version)
         {
             // initialize a KeyVault client with a managed identity-based authentication callback
             var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback((a, r, s) => { return AuthenticationCallbackAsync(a, r, s); }));
@@ -123,31 +188,31 @@ namespace Azure.ServiceFabric.ManagedIdentity.Samples
             Log(LogLevel.Info, $"\n== {DateTime.UtcNow.ToString()}: Probing secret...");
             try
             {
-                var secretResponse = await kvClient.GetSecretWithHttpMessagesAsync(vault, secret, version)
+                var secretResponse = await kvClient.GetSecretWithHttpMessagesAsync(vaultUri, Name, version)
                     .ConfigureAwait(false);
 
                 if (secretResponse.Response.IsSuccessStatusCode)
                 {
                     // use the secret: secretValue.Body.Value;
-                    response = String.Format($"Successfully probed secret '{secret}' in vault '{vault}': {PrintSecretBundleMetadata(secretResponse.Body)}");
+                    response = String.Format($"Successfully probed secret '{Name}' in vault '{vaultUri}': {PrintSecretBundleMetadata(secretResponse.Body)}");
                 }
                 else
                 {
-                    response = String.Format($"Non-critical error encountered retrieving secret '{secret}' in vault '{vault}': {secretResponse.Response.ReasonPhrase} ({secretResponse.Response.StatusCode})");
+                    response = String.Format($"Non-critical error encountered retrieving secret '{Name}' in vault '{vaultUri}': {secretResponse.Response.ReasonPhrase} ({secretResponse.Response.StatusCode})");
                 }
             }
             catch (Microsoft.Rest.ValidationException ve)
             {
-                response = String.Format($"encountered REST validation exception 0x{ve.HResult.ToString("X")} trying to access '{secret}' in vault '{vault}' from {ve.Source}: {ve.Message}");
+                response = String.Format($"encountered REST validation exception 0x{ve.HResult.ToString("X")} trying to access '{Name}' in vault '{vaultUri}' from {ve.Source}: {ve.Message}");
             }
             catch (KeyVaultErrorException kvee)
             {
-                response = String.Format($"encountered KeyVault exception 0x{kvee.HResult.ToString("X")} trying to access '{secret}' in vault '{vault}': {kvee.Response.ReasonPhrase} ({kvee.Response.StatusCode})");
+                response = String.Format($"encountered KeyVault exception 0x{kvee.HResult.ToString("X")} trying to access '{Name}' in vault '{vaultUri}': {kvee.Response.ReasonPhrase} ({kvee.Response.StatusCode})");
             }
             catch (Exception ex)
             {
                 // handle generic errors here
-                response = String.Format($"encountered exception 0x{ex.HResult.ToString("X")} trying to access '{secret}' in vault '{vault}': {ex.Message}");
+                response = String.Format($"encountered exception 0x{ex.HResult.ToString("X")} trying to access '{Name}' in vault '{vaultUri}': {ex.Message}");
             }
 
             Log(LogLevel.Info, response);
@@ -211,16 +276,32 @@ namespace Azure.ServiceFabric.ManagedIdentity.Samples
         {
             StringBuilder strBuilder = new StringBuilder();
 
-            strBuilder.AppendFormat($"\n\tid: {bundle.Id}\n");
+            strBuilder.AppendFormat($"id: {bundle.Id}\n");
             strBuilder.AppendFormat($"\tcontent type: {bundle.ContentType}\n");
             strBuilder.AppendFormat($"\tmanaged: {bundle.Managed}\n");
-            strBuilder.AppendFormat($"\tattributes:\n");
-            strBuilder.AppendFormat($"\t\tenabled: {bundle.Attributes.Enabled}\n");
-            strBuilder.AppendFormat($"\t\tnbf: {bundle.Attributes.NotBefore}\n");
-            strBuilder.AppendFormat($"\t\texp: {bundle.Attributes.Expires}\n");
-            strBuilder.AppendFormat($"\t\tcreated: {bundle.Attributes.Created}\n");
-            strBuilder.AppendFormat($"\t\tupdated: {bundle.Attributes.Updated}\n");
-            strBuilder.AppendFormat($"\t\trecoveryLevel: {bundle.Attributes.RecoveryLevel}\n");
+            strBuilder.AppendFormat($"\tenabled: {bundle.Attributes.Enabled}\n");
+            strBuilder.AppendFormat($"\tnbf: {bundle.Attributes.NotBefore}\n");
+            strBuilder.AppendFormat($"\texp: {bundle.Attributes.Expires}\n");
+            strBuilder.AppendFormat($"\tcreated: {bundle.Attributes.Created}\n");
+            strBuilder.AppendFormat($"\tupdated: {bundle.Attributes.Updated}\n");
+            strBuilder.AppendFormat($"\trecoveryLevel: {bundle.Attributes.RecoveryLevel}\n");
+
+            return strBuilder.ToString();
+        }
+
+        private string PrintKeyVaultSecretMetadata(KeyVaultSecret secret)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+
+            strBuilder.AppendFormat($"id: {secret.Id}\n");
+            strBuilder.AppendFormat($"\tcontent type: {secret.Properties.ContentType}\n");
+            strBuilder.AppendFormat($"\tmanaged: {secret.Properties.Managed}\n");
+            strBuilder.AppendFormat($"\tenabled: {secret.Properties.Enabled}\n");
+            strBuilder.AppendFormat($"\tnbf: {secret.Properties.NotBefore}\n");
+            strBuilder.AppendFormat($"\texp: {secret.Properties.ExpiresOn}\n");
+            strBuilder.AppendFormat($"\tcreated: {secret.Properties.CreatedOn}\n");
+            strBuilder.AppendFormat($"\tupdated: {secret.Properties.UpdatedOn}\n");
+            strBuilder.AppendFormat($"\trecoveryLevel: {secret.Properties.RecoveryLevel}\n");
 
             return strBuilder.ToString();
         }
